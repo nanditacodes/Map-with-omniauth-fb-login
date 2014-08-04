5d87f6904e19852c99e8afacdeb6c344f6b8f304
Nandita Rajagopalan <nandita@Nanditas-MacBook-Pro.local>
2014-08-04 14:47:59 -0500

    persist already pinned maps

--- app/assets/javascripts/marker.js
+++ app/assets/javascripts/marker.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-
+  var myArray =  $('#markers').data('url')
   window.map = L.map('map').setView([29.7601927,-95.36938959999999], 10);
   L.tileLayer('https://{s}.tiles.mapbox.com/v3/{id}/{z}/{x}/{y}.png', {
     maxZoom: 18,
@@ -8,4 +8,9 @@ $(document).ready(function() {
       'Imagery © <a href="http://mapbox.com">Mapbox</a>',
     id: 'examples.map-i86knfo3'
   }).addTo(map);
+
+  for (var i=0; i < myArray.length; i++){
+    L.marker([myArray[i].latitude, myArray[i].longitude]).addTo(map)
+  }
+
 });
